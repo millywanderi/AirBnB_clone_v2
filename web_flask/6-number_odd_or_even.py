1cd4047750c6d0f79cd6038fb0f28d85c535ec03
millywanderi <wanderinyambura5@gmail.com>
2023-09-22 16:51:14 +0300

    added t at route

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,7 +49,7 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.roue('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """displays html if n is an int"""
     return render_template("6-number_odd_or_even.html", n=n)
